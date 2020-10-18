40c3e34a78d8ac617015358880a55cfa6f8fc46e
Heiko Goes <hgoes@aidminutes.com>
2020-10-18 11:57:07 +0200

    Map.update(acc, char, 1, &(&1 + 1)) instead of complicated  case fetch

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -7,21 +7,20 @@ defmodule WordCount do
   @spec count(String.t()) :: map
   def count(sentence) do
     String.downcase(sentence)
-      |> String.replace("_", " ")
-      |> ignore_punctuation()
-      |> String.split(" ", trim: true)
-      |> Enum.reduce(%{}, fn x, acc ->
-        case Map.fetch(acc, x) do
-          {:ok, count} -> Map.put(acc, x, count + 1)
-          _ -> Map.put_new(acc, x, 1)
-        end
-      end )
+    |> String.replace("_", " ")
+    |> ignore_punctuation()
+    |> String.split(" ", trim: true)
+    |> Enum.reduce(%{}, fn char, acc ->
+      Map.update(acc, char, 1, &(&1 + 1))
+    end)
   end
 
   defp ignore_punctuation(word) do
     punctuation = String.codepoints(",:!&@$%^&)\"")
+
     Enum.filter(String.codepoints(word), fn character ->
-      character not in punctuation end)
+      character not in punctuation
+    end)
     |> List.to_string()
   end
 end
