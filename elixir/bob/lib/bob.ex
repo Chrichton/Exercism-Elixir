4ead4fa566e2850008a4c8130ab3c3f511941ff1
Heiko Goes <hgoes@aidminutes.com>
2020-10-25 12:01:20 +0100

    1 failure

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -9,7 +9,7 @@ defmodule Bob do
       question_with_non_letter_characters?(input) -> "Sure."
       asking_in_capitals?(input) -> "Calm down, I know what I'm doing!"
       String.last(input) == "?" -> "Sure."
-
+      talking_forcefully(input) -> "Whatever."
       shouting_in_capitals(input) -> "Whoa, chill out!"
       talking_in_capitals?(input) -> "Whatever."
       String.last(input) == "!" -> "Whoa, chill out!"
@@ -17,9 +17,14 @@ defmodule Bob do
     end
   end
 
+  defp talking_forcefully(sentence),
+    do: String.last(sentence) == "!" && !all_words_capitals?(sentence)
+
   defp shouting_in_capitals(sentence), do: String.upcase(sentence) == sentence
 
-  defp talking_in_capitals?(sentence) do
+  defp talking_in_capitals?(sentence), do: all_words_capitals?(sentence)
+
+  defp all_words_capitals?(sentence) do
     sentence
     |> String.split(" ")
     |> Enum.all?(fn word ->
@@ -31,14 +36,14 @@ defmodule Bob do
   defp asking_in_capitals?(sentence),
     do: shouting_in_capitals(sentence) && String.last(sentence) == "?"
 
-   defp question_with_non_letter_characters?(sentence) do
+  defp question_with_non_letter_characters?(sentence) do
     String.last(sentence) == "?" && String.contains?(sentence, @non_letter_characters)
-   end
+  end
 
   defp question_with_numbers?(sentence),
     do:
       String.last(sentence) == "?" &&
-        contains_numbers_only?(String.slice(sentence, 0..String.length(sentence) - 2))
+        contains_numbers_only?(String.slice(sentence, 0..(String.length(sentence) - 2)))
 
   defp contains_numbers_only?(sentence) do
     case Integer.parse(sentence) do
