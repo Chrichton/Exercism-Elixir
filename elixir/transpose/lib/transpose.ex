34bd26bb4e5d881bb0f53cfa0cdd0c27d93aadb0
Heiko Goes <hgoes@aidminutes.com>
2020-11-25 21:54:15 +0100

    transpose_recursive

--- elixir/transpose/lib/transpose.ex
+++ elixir/transpose/lib/transpose.ex
@@ -19,8 +19,35 @@ defmodule Transpose do
   @spec transpose(String.t()) :: String.t()
   def transpose(input) do
     input
-    |> String.split("\n")
-    |> Enum.map(&(String.codepoints(&1)) |> Enum.join("\n"))
-    |> Enum.join()
+    |> to_rows()
+    |> transpose_recursive()
+    |> Enum.join("\n")
   end
+
+  def transpose_recursive(rows) do
+    {row, remaining_rows} = transpose_first_column(rows)
+
+    if Enum.flat_map(remaining_rows, fn x -> x end) == [],
+      do: row,
+      else: [row | transpose_recursive(remaining_rows)]
+  end
+
+  # "ab\ncd" -> [["a", "b"], ["c", "d"]]
+  def to_rows(input) do
+    input
+    |> String.split("\n")
+    |> pad_right()
+    |> Enum.map(&String.codepoints/1)
+  end
+
+  def pad_right(lines) do
+    max_line_length = Enum.max_by(lines, &String.length/1) |> String.length()
+    Enum.map(lines, fn line -> String.pad_trailing(line, max_line_length) end)
+  end
+
+  # empty list
+  def transpose_first_column([[]]), do: {[], []}
+
+  def transpose_first_column(rows),
+    do: {Enum.map(rows, fn [head | _] -> head end), Enum.map(rows, fn [_ | tail] -> tail end)}
 end
