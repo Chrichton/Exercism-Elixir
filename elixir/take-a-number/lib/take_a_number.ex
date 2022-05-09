962d6104e37441cc4014ad0f918a93762c0baecd
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-05-09 14:30:55 +0200

    all tests pass

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,24 @@
 defmodule TakeANumber do
+  def loop(counter \\ 0) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, counter)
+        loop(counter)
+
+      {:take_a_number, sender_pid} ->
+        counter = counter + 1
+        send(sender_pid, counter)
+        loop(counter)
+
+      :stop ->
+        counter
+
+      _ ->
+        loop(counter)
+    end
+  end
+
   def start() do
-    # Please implement the start/0 function
+    spawn(&loop/0)
   end
 end
